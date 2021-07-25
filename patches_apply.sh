3e40c0cf8ed324aa0afc2642f21410c43d179a31
MSe1969 <mse1969@posteo.de>
2021-07-25 21:08:37 +0200

    Add patches for Constify JNI method tables
    Change-Id: I7c4a4549dcad639500ab23385dc11c98ba2ef6ff

--- patches_apply.sh
+++ patches_apply.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 
+list_repos() {
+cat <<EOF
+external/ant-wireless/ant_native:patch_702_ant-wireless.patch
+external/conscrypt:patch_703_conscrypt.patch
+external/icu:patch_704_icu.patch
+external/neven:patch_705_neven.patch
+frameworks/rs:patch_706_rs.patch
+frameworks/ex:patch_707_ex.patch
+frameworks/opt/net/voip:patch_708_voip.patch
+hardware/qcom-caf/common:patch_709_qc-common.patch
+lineage-sdk:patch_710_lineage-sdk.patch
+packages/apps/FMRadio:patch_711_FMRadio.patch
+packages/apps/Gallery2:patch_712_Gallery2.patch
+packages/apps/Terminal:patch_715_Terminal.patch
+vendor/qcom/opensource/fm-commonsys:patch_716_fm-commonsys.patch
+vendor/nxp/opensource/commonsys/packages/apps/Nfc:patch_717_nxp-Nfc.patch
+vendor/qcom/opensource/libfmjni:patch_718_libfmjni.patch
+EOF
+}
+
+
 THISDIR=$PWD
 
 cd ..
@@ -24,5 +45,15 @@ echo "Patching $PWD (Suggest disabled metrics by default)"
 patch -p1 < $THISDIR/patch_102_SetupWizard.patch
 echo "-"
 cd $TOPDIR
+
+list_repos | while read STR; do
+  DIR=$(echo $STR | cut -f1 -d:)
+  PTC=$(echo $STR | cut -f2 -d:)
+  cd $DIR
+  echo "Constify JNI method tables: $DIR"
+  patch -p1 < $THISDIR/$PTC
+  cd $TOPDIR
+done
+
 
 cd $THISDIR
--- patches_reverse.sh
+++ patches_reverse.sh
@@ -18,7 +18,22 @@ cd ..
 TOPDIR=$PWD
 
 clear_set device/common
+clear_set external/ant-wireless/ant_native
+clear_set external/conscrypt
+clear_set external/icu
+clear_set external/neven
+clear_set frameworks/ex
+clear_set frameworks/opt/net/voip
+clear_set frameworks/rs
+clear_set hardware/qcom-caf/common
+clear_set lineage-sdk
 clear_set packages/apps/Dialer
+clear_set packages/apps/FMRadio
+clear_set packages/apps/Gallery2
 clear_set packages/apps/SetupWizard
+clear_set packages/apps/Terminal
+clear_set vendor/nxp/opensource/commonsys/packages/apps/Nfc
+clear_set vendor/qcom/opensource/fm-commonsys
+clear_set vendor/qcom/opensource/libfmjni
 
 cd $THISDIR
