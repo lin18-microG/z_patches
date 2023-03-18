b6dc0b31302db73e823b1d126a7a0cb0d1c7a1ce
MSe <mse1969@posteo.de>
2023-03-18 20:27:22 +0100

    Webview switcher patch
    Mulch and the geuinie LineageOS webview use the same module name, which
    causes build errors. Both repositories however are needed - the default
    build uses the LineageOS webview, the microG builds use Mulch.
    
    To solve the conflict, a patch is applied to the respective "unused"
    repository to rename the module to an 'unused' one.
    
    Change-Id: I2d0ed8152f569195b693a6db09a7460e100bb609

--- patches_apply.sh
+++ patches_apply.sh
@@ -43,6 +43,12 @@ patch -p1 < $THISDIR/patch_200_openssh.patch
 echo "-"
 cd $TOPDIR
 
+cd external/chromium-webview/patches
+echo "Patching $PWD (webview-switch)"
+patch -p1 < $THISDIR/patch_400_webview.patch
+echo "-"
+cd $TOPDIR
+
 cd packages/apps/Dialer
 echo "Patching $PWD (Remove Google forward lookup)"
 patch -p1 < $THISDIR/patch_101_Dialer.patch
